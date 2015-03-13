ac36eea2084ac29d537d1c79ae8ec4f34b04d9e2
James <hzhan12@hotmail.com>
2015-03-13 13:55:44 -0400

    * Integrate http://getbootstrap.com/ to make it look nice * Use one template for the header and footer that are included in all pages

--- yaas/src/main/java/arvato/yaas/config/ApplicationConfigurerAdapter.java
+++ yaas/src/main/java/arvato/yaas/config/ApplicationConfigurerAdapter.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -12,16 +13,22 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class ApplicationConfigurerAdapter extends WebMvcConfigurerAdapter{
 
     @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("WEB-INF/jsp/");
         resolver.setSuffix(".jsp");
         return resolver;
     }
+
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry)
+	{
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
 
 }
